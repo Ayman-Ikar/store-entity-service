ba03f8c5f88d33912e9f6d8c684105e40ff1312d
Ayman Ikar <37595519+Ayman-Ikar@users.noreply.github.com>
2023-01-22 20:23:24 +0100

    Entity service Database properties +

---
+++ src/main/java/com/store/entityservice/configuration/EStoreDatabaseConfiguration.java
@@ -0,0 +1,36 @@
+package com.store.entityservice.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class EStoreDatabaseConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String className;
+
+
+    @Bean
+    public DataSource getDataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName(className)
+                .url(url)
+                .username(username)
+                .password(password)
+                .build();
+    }
+
+}
