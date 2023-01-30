d3283df0e2ee3db35606dca35a9f607f61fc89c7
Ayman Ikar <37595519+Ayman-Ikar@users.noreply.github.com>
2023-01-30 16:58:31 +0100

    registering with eureka

--- src/main/java/com/store/entityservice/configuration/EStoreDatabaseConfiguration.java
+++ src/main/java/com/store/entityservice/configuration/EStoreDatabaseConfiguration.java
@@ -2,12 +2,14 @@ package com.store.entityservice.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableDiscoveryClient
 public class EStoreDatabaseConfiguration {
 
     @Value("${spring.datasource.url}")
